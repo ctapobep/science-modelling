2598f8622bab5a65f49d32460236344da36fc256
sbashkyrtsev <stanislav.bashkyrtsev@kohls.com>
2014-06-28 20:33:27 +0400

    paracetamol

--- physiology/src/main/groovy/edu/science/biology/pharmacology/AntiInflammatoryDrugs.groovy
+++ physiology/src/main/groovy/edu/science/biology/pharmacology/AntiInflammatoryDrugs.groovy
@@ -11,14 +11,29 @@ class AntiInflammatoryDrugs {
         effect       : "Gets you free from fever, inflammation, pain, reduces blood clotting",
         adverseEffect: "Irritates GI tract"],
       iboprofin: "Very similar to aspirin"],
-    ParaAminoPhenols: [
-      description               : 'Not much difference in pain relief comparing to Salicylate Family, but side effects are different',
-      ["paracetomol", "tylenol"]: [
-        aka: "acetaminophen or APAP",
-        effect       : "Pain reliever, but doesn't reduce blood clotting or inflammation",
-        adverseEffect: "Is bad for liver"]],
+    ParAminoPhenols : [
+      description                                   : 'Not much difference in pain relief comparing to Salicylate Family, but side effects are different',
+      ["paracetomol", "tylenol(R)", "acetaminophen"]: [
+        aka          : "APAP",
+        chemistry    : "N-acetly-para-aminophenol",
+        effect       : ["Blocks the synthesis of prostaglandins centrally (but not peripherally).",
+                        "Pain reliever, decreases fever",
+                        "doesn't reduce blood clotting"],
+        adventages   : ["not that allergic as aspirin"],
+        disadvanteges: "less anti-inflammatory effect than with aspirin",
+        adverseEffect: ["Is bad for liver", "can be a problem for diabetics"]]],
     Excedrin        : [
       effect: "Analgin+Paracetomol. Thus contains the same amount of pain relief, but hurts liver/GI less."
+    ],
+    cox2nsaids      : [
+      description : "cause less GI irritation",
+      allopurinol : [
+        conditions: ["gouty arthritis (uric acid in joints)", 'kidney stones']
+      ],
+      methotrexate: [
+        conditions   : ['rheumatoid arthritis'],
+        adverseEffect: ['ulcerative stomatitis', 'gingivitis']
+      ]
     ]
   ]
 }
