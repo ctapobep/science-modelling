78de06664e724ab11b9d2dfc006e4ee7c786be4f
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-12-02 20:14:26 +0300

    Added Confidence Interval.

--- statistics/src/main/groovy/edu/science/statistics/Measures.groovy
+++ statistics/src/main/groovy/edu/science/statistics/Measures.groovy
@@ -1,21 +1,38 @@
 package edu.science.statistics
 
 class Measures {
+  def zTable = [confidence: 'multiplier']
+  def tTable = ['sampleSize-1': [confidence: 'multiplier']]
+
   class ContinuousData {
     List<Integer> sample = [1, 4, 12, 5, 119, 123, 0]
     double mean = sample.sum() / sample.size()
     double variance = sample.every { Math.pow(mean - it, 2) } / (sample.size() - 1)
     double standardDeviation = Math.sqrt(variance)
     double standardError = standardDeviation/Math.sqrt(sample.size())
+    /** 2 can be replaced with value from {@link Measures#zTable} if not 95% CI is required */
+    def largeSizeConfidenceInterval = [mean - 2 * standardError, mean + 2 * standardError]
+    /**
+     * 2 MUST be replaced with value from {@link Measures#tTable} to adjust for
+     * small sample size and to required CI.
+     */
+    def smallSizeConfidenceInterval = [mean - 2 * standardError, mean + 2 * standardError]
   }
 
   class BinomialData {
     List<Boolean> sample = [true, true, false, false, true, false, true, true, true]
     /** aka mean */
-    double proportion = sample.find { it } / sample.size()
-    double variance = sample.find { it } * sample.find { !it }
+    double proportion = sample.findAll { it }.size() / sample.size()
+    double variance = proportion * (1 - proportion)
     double standardDeviation = Math.sqrt(variance)
     double standardError = standardDeviation/Math.sqrt(sample.size())
+    /** 2 can be replaced with value from {@link Measures#zTable} if not 95% CI is required */
+    def largeSizeConfidenceInterval = [proportion - 2 * standardError, proportion + 2 * standardError]
+    /**
+     * 2 MUST be replaced with value from {@link Measures#tTable} to adjust for
+     * small sample size and to required CI.
+     */
+    def smallSizeConfidenceInterval = [proportion - 2 * standardError, proportion + 2 * standardError]
   }
 
   class TimeToEventData {
