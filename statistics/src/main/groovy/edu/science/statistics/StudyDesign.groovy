02e66c1cf0899242cbb2a994eddfa4daba63c074
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-12-02 20:19:27 +0300

    Refactored study designs.

--- statistics/src/main/groovy/edu/science/statistics/StudyDesign.groovy
+++ statistics/src/main/groovy/edu/science/statistics/StudyDesign.groovy
@@ -4,31 +4,35 @@ class StudyDesign {
   /** Just measuring an exposure in a sample. Ex: conducting a poll of how much sugar people take per day */
   class SampleStudy {}
   /** Looking for relation between variables in sample. Ex: comparing sugar intake and heart disease. */
-  class ObservationalStudy{}
+  class ObservationalStudy {
+    /**
+     * Retrospective studies. We have an outcome (disease) and look retrospectively at whether the exposure (smoking)
+     * was present or not.
+     */
+    class CaseControl {
+      double oddsRatio
+    }
+  }
   /**
    * Intentionally exposing or not a sample of people and see how it will work. Ex: separating 2 samples of people into
    * exposed & control groups, giving the exposed group some amount of sure and compare these groups in couple of years
    * on the rate of heart disease.
    */
-  class Experiment{}
+  class Experiment {
+    /** Population is divided into groups by exposure and waiting whether the outcome was achieved. */
+    class Cohort {
+      def data = Measures.TimeToEventData
+      double incidenceRate
+    }
 
-  /** Population is divided into groups by exposure and waiting whether the outcome was achieved. */
-  class Cohort {
-    double incidenceRatio
-  }
-  /**
-   * Retrospective studies. We have an outcome (disease) and look retrospectively at whether the exposure (smoking)
-   * was present or not.
-   */
-  class CaseControl {
-    double oddsRatio
+    class RandomizedControlledStudy {
+      double absoluteRiskDifference
+      /**
+       * Risk Ratio
+       */
+      double relativeRisk
+    }
   }
 
-  class RandomizedControlledStudy {
-    double absoluteRiskDifference
-    /**
-     * Risk Ratio
-     */
-    double relativeRisk
-  }
+
 }
