7cd94bd3a05e1aec377f24ce703fededff69934b
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-12-02 20:29:47 +0300

    Comparing means of 2 samples with CI.

--- statistics/src/main/groovy/edu/science/statistics/Measures.groovy
+++ statistics/src/main/groovy/edu/science/statistics/Measures.groovy
@@ -1,5 +1,8 @@
 package edu.science.statistics
 
+import static java.lang.Math.pow
+import static java.lang.Math.sqrt
+
 class Measures {
   def zTable = [confidence: 'multiplier']
   def tTable = ['sampleSize-1': [confidence: 'multiplier']]
@@ -7,9 +10,9 @@ class Measures {
   class ContinuousData {
     List<Integer> sample = [1, 4, 12, 5, 119, 123, 0]
     double mean = sample.sum() / sample.size()
-    double variance = sample.every { Math.pow(mean - it, 2) } / (sample.size() - 1)
-    double standardDeviation = Math.sqrt(variance)
-    double standardError = standardDeviation/Math.sqrt(sample.size())
+    double variance = sample.every { pow(mean - it, 2) } / (sample.size() - 1)
+    double standardDeviation = sqrt(variance)
+    double standardError = standardDeviation / sqrt(sample.size())
     /** 2 can be replaced with value from {@link Measures#zTable} if not 95% CI is required */
     def largeSizeConfidenceInterval = [mean - 2 * standardError, mean + 2 * standardError]
     /**
@@ -24,8 +27,8 @@ class Measures {
     /** aka mean */
     double proportion = sample.findAll { it }.size() / sample.size()
     double variance = proportion * (1 - proportion)
-    double standardDeviation = Math.sqrt(variance)
-    double standardError = standardDeviation/Math.sqrt(sample.size())
+    double standardDeviation = sqrt(variance)
+    double standardError = standardDeviation / sqrt(sample.size())
     /** 2 can be replaced with value from {@link Measures#zTable} if not 95% CI is required */
     def largeSizeConfidenceInterval = [proportion - 2 * standardError, proportion + 2 * standardError]
     /**
@@ -38,4 +41,15 @@ class Measures {
   class TimeToEventData {
 
   }
+
+  def sample1 = new ContinuousData()
+  def sample2 = new ContinuousData()
+  /**
+   * Can be used for any type of data, but let's describe only continuous.
+   * {@code 2} should be adjusted for small size (t-table must be used) or/and for required confidence (e.g. !=95%).
+   */
+  def comparingTwoSamplesMeans = [
+    (sample1.mean - sample2.mean) - 2 * sqrt(pow(sample1.standardError, 2) + pow(sample2.standardError, 2)),
+    (sample1.mean - sample2.mean) + 2 * sqrt(pow(sample1.standardError, 2) + pow(sample2.standardError, 2))
+  ]
 }
