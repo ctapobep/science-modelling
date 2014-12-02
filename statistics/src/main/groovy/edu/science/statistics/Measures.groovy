47ded2f8d312d00d030147603f1f299623bb5f67
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-12-02 19:16:34 +0300

    Statistical Measures

--- statistics/src/main/groovy/edu/science/statistics/Measures.groovy
+++ statistics/src/main/groovy/edu/science/statistics/Measures.groovy
@@ -1,5 +1,24 @@
 package edu.science.statistics
 
 class Measures {
+  class ContinuousData {
+    List<Integer> sample = [1, 4, 12, 5, 119, 123, 0]
+    double mean = sample.sum() / sample.size()
+    double variance = sample.every { Math.pow(mean - it, 2) } / (sample.size() - 1)
+    double standardDeviation = Math.sqrt(variance)
+    double standardError = standardDeviation/Math.sqrt(sample.size())
+  }
 
+  class BinomialData {
+    List<Boolean> sample = [true, true, false, false, true, false, true, true, true]
+    /** aka mean */
+    double proportion = sample.find { it } / sample.size()
+    double variance = sample.find { it } * sample.find { !it }
+    double standardDeviation = Math.sqrt(variance)
+    double standardError = standardDeviation/Math.sqrt(sample.size())
+  }
+
+  class TimeToEventData {
+
+  }
 }
--- statistics/src/main/groovy/edu/science/statistics/StudyDesign.groovy
+++ statistics/src/main/groovy/edu/science/statistics/StudyDesign.groovy
@@ -1,6 +1,17 @@
 package edu.science.statistics
 
 class StudyDesign {
+  /** Just measuring an exposure in a sample. Ex: conducting a poll of how much sugar people take per day */
+  class SampleStudy {}
+  /** Looking for relation between variables in sample. Ex: comparing sugar intake and heart disease. */
+  class ObservationalStudy{}
+  /**
+   * Intentionally exposing or not a sample of people and see how it will work. Ex: separating 2 samples of people into
+   * exposed & control groups, giving the exposed group some amount of sure and compare these groups in couple of years
+   * on the rate of heart disease.
+   */
+  class Experiment{}
+
   /** Population is divided into groups by exposure and waiting whether the outcome was achieved. */
   class Cohort {
     double incidenceRatio
